289ea52cc5b602716f8e100d70e9cd26ffa9c23a
c0d-0x <c0d_0x007@proton.me>
2024-04-23 22:16:15 +0100

    refactoring

---
+++ filemond.c
@@ -0,0 +1,21 @@
+#include "filemond.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *load_file(char *file_Path) {
+  char *saveptr, *temp;
+  char *file_name = malloc(sizeof(uint16_t *));
+  temp = strtok_r(file_Path, "/", &saveptr);
+  while (temp != NULL) {
+    file_name = temp;
+    temp = strtok_r(NULL, "/", &saveptr);
+  }
+  if (file_name != NULL && file_Path != NULL) {
+    return file_name;
+  }
+
+  return NULL;
+}
+
+char *find_sub_dir(char *directory) {}
